8bef59abadaf8f7d078684c81848ae3710577721
Isaac Valadez <isaac@ziqquforge.com>
2015-04-02 23:53:36 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AceInternational::Application.config.secret_key_base = 'de76571d63af09f598576bf695829c8dab30fab7673c8c96997dd4f762d574aa43304498f2f6b5e22e22683c67e14135086c8fc7cb906179c7e780e262dd1301'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AceInternational::Application.config.session_store :cookie_store, key: '_ace_international_session'
