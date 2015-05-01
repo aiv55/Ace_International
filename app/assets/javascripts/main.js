c454c5d158bff94c360449c6e9657a4c6cf1013d
Isaac Valadez <isaac@ziqquforge.com>
2015-04-30 21:44:20 -0500

    Finish mobile version styling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,50 @@
+$( document ).ready(function(){
+	
+	//show dropzone field
+	$('#dropzonePreview').show();
+	
+	//navigation becomes fixed when you scroll down
+	var navElement = $('nav ul');
+	var contentTop = $('#body-content');
+    var navOffset = navElement.offset().top;
+    var scrollOffset; 
+    function setNavPosition() {
+    	scrollOffset = $(this).scrollTop();   	
+      	var fix = (scrollOffset > navOffset) ? true : false;
+      	navElement.toggleClass("fixed", fix);
+		$('body').toggleClass("fixed-nav", fix);
+	};
+    function resetNavOffset() {
+    	if (navElement.hasClass('fixed')) {
+    		navOffset = contentTop.offset().top;
+    		navOffset += - 34;
+    	} else {
+    		navOffset = navElement.offset().top;
+    	};
+	};
+    $(window).scroll( setNavPosition );
+    $(window).resize(function() {
+    	var id;
+    	clearTimeout(id);
+    	id = setTimeout(resetNavOffset, 500);    
+	});//end resize
 
+	//smoothScroll
+	$('nav a').smoothScroll();
 
-// phone input field auto-focus
-$('.phoneInput input').keyup(function(){
-	if(this.value.length==this.attr("size")){
-		if(this.attr("size") == 4){
-			this.parent().nextAll()[1].children().focus();
-		} else {
-			this.parent().nextAll()[1].focus();
-		}
-	}
-}); //end keyup event.
+	//phone input fields automatically shift focus to next input
+	$('.phoneInput input').keyup(function(){
+		
+		var input = $(this).val().length.toString();
+		var fieldSize = $(this).attr("size");
+		
+		if($(this).is("#phone1") && (input === fieldSize)){
+			$('#phone2').focus();
+		} else if($(this).is("#phone2") && (input === fieldSize)) {
+			$('#phone3').focus();
+		} else if(input === fieldSize) {
+			$('#email').focus();
+		};
+	}); //end keyup event
+	
+}); //end ready
